7f873b7b91aafa2d26ea10d938a3b08b09ec339c
Rutger Smit <rutger.smit@gmail.com>
2021-12-08 00:07:00 +0100

    Massive update of ... everything

--- logic/screenshotter.py
+++ logic/screenshotter.py
@@ -1,28 +1,20 @@
 import os
 import time
-import datetime
 
 from os import path
 from PIL import Image
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class Screenshotter():
-
     driver = None
 
-    def __init__(self):
-        self.init_driver()
-
-    def __del__(self):
+    def __del__(self) -> None:
         driver.quit()
 
-    def init_driver(self):
+    def init_driver(self, selniumserver) -> None:
         # initialize the driver that connects to the selenium container
-        #print('Init driver')
-
         options = Options()
         options.headless = True
         options.add_argument("--log-level=3")
@@ -31,23 +23,35 @@ class Screenshotter():
         options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4501.0 Safari/537.36 Edg/92.0.891.1')
 
         global driver
+
         driver = webdriver.Remote(
             options=options,
-            command_executor='http://{}:4444/wd/hub'.format('192.168.100.10'), # TODO: make configurable
+            command_executor=selniumserver,
             desired_capabilities=DesiredCapabilities.CHROME)
 
-    def screenshot(self, id):
         window_size = driver.execute_script("""
-                    return [window.outerWidth - window.innerWidth + arguments[0],
-                        window.outerHeight - window.innerHeight + arguments[1]];
-                    """, 650, 2250)
+                            return [window.outerWidth - window.innerWidth + arguments[0],
+                                window.outerHeight - window.innerHeight + arguments[1]];
+                            """, 1650, 4250)
         driver.set_window_size(*window_size)
 
-        driver.get(f"https://twitter.com/thierrybaudet/status/{id}")
-        time.sleep(5)
 
-        driver.save_screenshot("pdata/page.png")
-        element = driver.find_element_by_css_selector('article[data-testid="tweet"]')
+    def screenshot(self, tweet) -> str:
+        username = tweet.author.screen_name
+        id = str(tweet.id)
+
+        driver.get(f"https://twitter.com/{username}/status/{id}")
+
+        time.sleep(3) #really? Yes, really! Need this time to load data async.
+        driver.execute_script("window.scrollTo(0, 0);")
+
+        datadir = f"data/{username}/"
+        if not path.isdir(datadir):
+            os.makedirs(datadir)
+
+        driver.save_screenshot(f"{datadir}page.png")
+        element = driver.find_element_by_css_selector(
+            'article[data-testid="tweet"]:not([tabindex])')  # :not([tabindex])
 
         location = element.location
         size = element.size
@@ -55,13 +59,15 @@ class Screenshotter():
         y = location['y']
         width = location['x']+size['width']
         height = location['y']+size['height']
-        im = Image.open('data/page.png')
+
+        im = Image.open(f"{datadir}page.png")
         im = im.crop((int(x), int(y), int(width), int(height)))
 
-        d = datetime.datetime.now()
-        p = f"data/screenshots/{d.strftime('%m')}/{d.strftime('%d')}"
+        p = f"{datadir}screenshots/{tweet.created_at.strftime('%m')}/{tweet.created_at.strftime('%d')}"
 
         if not path.isdir(p):
             os.makedirs(p)
 
         im.save(f"{p}/{id}.png")
+
+        return f"{p}/{id}.png"
--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 from os import path
-from credentials import *
 import tweepy
+import logic.config
 
-import secrets
-
+from credentials import *
 from logic.screenshotter import Screenshotter
 
-last_id_file = 'data/last_id.txt'
 
-def check_paths():
-    if not path.isdir('data/screenshots'):
-        os.makedirs('data/screenshots')
+def get_last_id(username) -> int:
+    n = f"data/{username}/last_id_file.dat"
+    if not os.path.isfile(n):
+        set_last_id(username, 1)
 
-def get_last_id() -> int:
-    if not os.path.isfile(last_id_file):
-        set_last_id(1)
-
-    with open(last_id_file, 'r') as f:
+    with open(n, 'r') as f:
         val = f.readline()
         f.close()
         
         return int(val)
 
-def set_last_id(id) -> None:
-    with open(last_id_file, 'w') as f:
+
+def set_last_id(username, id) -> None:
+    n = f"data/{username}/last_id_file.dat"
+    with open(n, 'w') as f:
         f.write(str(id))
         f.close()
 
-def run():
-    userID = "thierrybaudet"
 
-    auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
-    auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+def run():
+    username = logic.config.get_config('twitterhandle')
+    seleniumurl = logic.config.get_config('seleniumurl')
+    consumer_key = logic.config.get_config('consumer_key')
+    consumer_secret = logic.config.get_config('consumer_secret')
+    access_token = logic.config.get_config('access_token')
+    access_token_secret = logic.config.get_config('access_token_secret')
+
+    if not path.isdir(f"data/{username}"):
+        os.makedirs(f"data/{username}")
+
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    since_id = get_last_id()
+    since_id = get_last_id(username)
 
-    tweets = api.user_timeline(screen_name=userID,
+    tweets = api.user_timeline(screen_name=username,
                             # 200 is the maximum allowed count
                             count=200,
                             include_rts=False,
@@ -45,18 +52,76 @@ def run():
                             #tweet_mode='extended',
                             since_id=since_id
                             )
-    shotter = Screenshotter()
-    shotter.init_driver()
 
-    for info in tweets:
-        print("ID: {}".format(info.id))
-        print(info.created_at)
-        print(info.text)
+    shotter = Screenshotter()
+    shotter.init_driver(seleniumurl)
+    
+    tweets.reverse()
+    for tweet in tweets:
+        print("ID: {}".format(tweet.id))
+        print(tweet.created_at)
+        print(tweet.text)
         print("\n")
 
-        shotter.screenshot(info.id)
+        #loc = shotter.screenshot(info.author.screen_name, info.id)
+        loc = shotter.screenshot(tweet)
+        print(f"Saved at {loc}", flush=True)
 
-    set_last_id(tweets[0].id)
+    if len(tweets) > 0:
+        set_last_id(username, tweets[0].id)
+    else:
+        print(f"No new tweets for {username}")
+
+def test():
+    seleniumurl = logic.config.get_config('seleniumurl')
+    consumer_key = logic.config.get_config('consumer_key')
+    consumer_secret = logic.config.get_config('consumer_secret')
+    access_token = logic.config.get_config('access_token')
+    access_token_secret = logic.config.get_config('access_token_secret')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    
+    shotter = Screenshotter()
+    shotter.init_driver(seleniumurl)
+
+    if not path.isdir('tests'):
+        os.makedirs('tests')
+
+
+    # original tweet without replies
+    print('original tweet without replies')
+    tweet = api.get_status(1466505804090122253)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/original_tweet_without_replies.png")
+
+    # original tweet with replies
+    print('original tweet with replies')
+    tweet = api.get_status(1467873464216440842)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/original_tweet_with_replies.png")
+
+    # reply on deleted tweet
+    print('reply on deleted tweet')
+    tweet = api.get_status(1454179653858603010)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/reply_on_deleted_tweet.png")
+
+    # reply on tweet with image
+    print('reply on tweet with image')
+    tweet = api.get_status(1454499417395015691)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/reply_on_tweet_with_image.png")
+
+    # reply on retweet with video
+    print('reply on retweet with video')
+    tweet = api.get_status(1454912491406905358)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/reply_on_retweet_with_video.png")
+
+
 
 if __name__ == '__main__':
     run()
+    #test()
