9dd41b43bc550661d0c5008ff08d811c2b308dd8
Rutger Smit <rutger.smit@gmail.com>
2022-01-09 22:42:09 +0100

    Added posting to Twitter

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from os import path
+import os
+import time
+import datetime
 import tweepy
 import logic.config
 
+from os import path
 from credentials import *
 from logic.screenshotter import Screenshotter
 
+
+username = logic.config.get_config('twitterhandle')
+seleniumurl = logic.config.get_config('seleniumurl')
+interval = int(logic.config.get_config('checkinterval'))
+consumer_key = logic.config.get_config('consumer_key')
+consumer_secret = logic.config.get_config('consumer_secret')
+access_token = logic.config.get_config('access_token')
+access_token_secret = logic.config.get_config('access_token_secret')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 def get_last_id(username) -> int:
     n = f"data/{username}/last_id_file.dat"
@@ -23,23 +38,11 @@ def set_last_id(username, id) -> None:
     with open(n, 'w') as f:
         f.write(str(id))
         f.close()
-
+    print(f"Saving id {id}")
 
 def run():
-    username = logic.config.get_config('twitterhandle')
-    seleniumurl = logic.config.get_config('seleniumurl')
-    consumer_key = logic.config.get_config('consumer_key')
-    consumer_secret = logic.config.get_config('consumer_secret')
-    access_token = logic.config.get_config('access_token')
-    access_token_secret = logic.config.get_config('access_token_secret')
-
     if not path.isdir(f"data/{username}"):
         os.makedirs(f"data/{username}")
-
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
 
     since_id = get_last_id(username)
 
@@ -58,70 +61,43 @@ def run():
     
     tweets.reverse()
     for tweet in tweets:
-        print("ID: {}".format(tweet.id))
-        print(tweet.created_at)
-        print(tweet.text)
-        print("\n")
+        try:
+            print(f"New tweet found: https://twitter.com/{tweet.author.screen_name}/status/{tweet.id}", flush=True)
+            loc = shotter.screenshot(tweet)
+            try:
+                post_screenshot(tweet, loc)
+            except:
+                print(f"Error posting screenshot", flush=True)
 
-        #loc = shotter.screenshot(info.author.screen_name, info.id)
-        loc = shotter.screenshot(tweet)
-        print(f"Saved at {loc}", flush=True)
+            set_last_id(username, tweet.id)
+
+        except:
+            print(f"Error screenshotting", flush=True)
 
     if len(tweets) > 0:
-        set_last_id(username, tweets[0].id)
+        print(f"Finished posting all new tweets of {username}", flush=True)
     else:
-        print(f"No new tweets for {username}")
+        print(f"No new tweets for {username}", flush=True)
 
-def test():
-    seleniumurl = logic.config.get_config('seleniumurl')
-    consumer_key = logic.config.get_config('consumer_key')
-    consumer_secret = logic.config.get_config('consumer_secret')
-    access_token = logic.config.get_config('access_token')
-    access_token_secret = logic.config.get_config('access_token_secret')
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    
-    
-    shotter = Screenshotter()
-    shotter.init_driver(seleniumurl)
-
-    if not path.isdir('tests'):
-        os.makedirs('tests')
-
-
-    # original tweet without replies
-    print('original tweet without replies')
-    tweet = api.get_status(1466505804090122253)
-    l = shotter.screenshot(tweet)
-    os.rename(l, "tests/original_tweet_without_replies.png")
-
-    # original tweet with replies
-    print('original tweet with replies')
-    tweet = api.get_status(1467873464216440842)
-    l = shotter.screenshot(tweet)
-    os.rename(l, "tests/original_tweet_with_replies.png")
-
-    # reply on deleted tweet
-    print('reply on deleted tweet')
-    tweet = api.get_status(1454179653858603010)
-    l = shotter.screenshot(tweet)
-    os.rename(l, "tests/reply_on_deleted_tweet.png")
-
-    # reply on tweet with image
-    print('reply on tweet with image')
-    tweet = api.get_status(1454499417395015691)
-    l = shotter.screenshot(tweet)
-    os.rename(l, "tests/reply_on_tweet_with_image.png")
-
-    # reply on retweet with video
-    print('reply on retweet with video')
-    tweet = api.get_status(1454912491406905358)
-    l = shotter.screenshot(tweet)
-    os.rename(l, "tests/reply_on_retweet_with_video.png")
-
-
+def post_screenshot(original_tweet, screenshot_path):
+    media = api.media_upload(screenshot_path)
+    tweet = api.update_status("", media_ids=[media.media_id], attachment_url=f"https://twitter.com/{original_tweet.author.screen_name}/status/{original_tweet.id}" )
+    print(f"Posted tweet screendump: https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}", flush=True)
 
 if __name__ == '__main__':
-    run()
-    #test()
+    print("""
+   _____ _     _ _     _______       _ _   _               _____                 
+  / ____| |   (_) |   |__   __|     (_) | | |             / ____|                
+ | (___ | |__  _| |_     | |_      ___| |_| |_ ___ _ __  | (___   __ _ _   _ ___ 
+  \___ \| '_ \| | __|    | \ \ /\ / / | __| __/ _ \ '__|  \___ \ / _` | | | / __|
+  ____) | | | | | |_     | |\ V  V /| | |_| ||  __/ |     ____) | (_| | |_| \__ \\
+ |_____/|_| |_|_|\__|    |_| \_/\_/ |_|\__|\__\___|_|    |_____/ \__,_|\__, |___/
+                                                                        __/ |    
+                                                                       |___/     
+    """, flush=True)
+    while True:
+        run()
+        next_check = datetime.datetime.now() + datetime.timedelta(0, interval)
+        print(
+            f"Sleeping for {interval} seconds. Next check at {next_check.strftime('%H:%M:%S')}", flush=True)
+        time.sleep(interval)
--- tests.py
+++ tests.py
@@ -5,60 +5,72 @@ import logic.config
 from credentials import *
 from logic.screenshotter import Screenshotter
 
-
-seleniumurl = logic.config.get_config('seleniumurl')
 consumer_key = logic.config.get_config('consumer_key')
 consumer_secret = logic.config.get_config('consumer_secret')
 access_token = logic.config.get_config('access_token')
 access_token_secret = logic.config.get_config('access_token_secret')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+def screenshot_tests():
+    seleniumurl = logic.config.get_config('seleniumurl')
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
 
-shotter = Screenshotter()
-shotter.init_driver(seleniumurl)
+    shotter = Screenshotter()
+    shotter.init_driver(seleniumurl)
 
-if not path.isdir('tests'):
-    os.makedirs('tests')
+    if not path.isdir('tests'):
+        os.makedirs('tests')
 
-# original tweet without replies
-print('original tweet without replies')
-tweet = api.get_status(1466505804090122253)
-l = shotter.screenshot(tweet)
-os.rename(l, "tests/original_tweet_without_replies.png")
+    # original tweet without replies
+    print('original tweet without replies')
+    tweet = api.get_status(1466505804090122253)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/original_tweet_without_replies.png")
 
-# # original tweet with replies
-# print('original tweet with replies')
-# tweet = api.get_status(1467873464216440842)
-# l = shotter.screenshot(tweet)
-# os.rename(l, "tests/original_tweet_with_replies.png")
+    # # original tweet with replies
+    # print('original tweet with replies')
+    # tweet = api.get_status(1467873464216440842)
+    # l = shotter.screenshot(tweet)
+    # os.rename(l, "tests/original_tweet_with_replies.png")
 
-# # reply on deleted tweet
-# print('reply on deleted tweet')
-# tweet = api.get_status(1454179653858603010)
-# l = shotter.screenshot(tweet)
-# os.rename(l, "tests/reply_on_deleted_tweet.png")
+    # # reply on deleted tweet
+    # print('reply on deleted tweet')
+    # tweet = api.get_status(1454179653858603010)
+    # l = shotter.screenshot(tweet)
+    # os.rename(l, "tests/reply_on_deleted_tweet.png")
 
-# # reply on tweet with image
-# print('reply on tweet with image')
-# tweet = api.get_status(1454499417395015691)
-# l = shotter.screenshot(tweet)
-# os.rename(l, "tests/reply_on_tweet_with_image.png")
+    # # reply on tweet with image
+    # print('reply on tweet with image')
+    # tweet = api.get_status(1454499417395015691)
+    # l = shotter.screenshot(tweet)
+    # os.rename(l, "tests/reply_on_tweet_with_image.png")
 
-# # quoted deleted tweet
-# print('quoted deleted tweet')
-# tweet = api.get_status(1448380249998303232)
-# l = shotter.screenshot(tweet)
-# os.rename(l, "tests/quoted_deleted_tweet.png")
+    # # quoted deleted tweet
+    # print('quoted deleted tweet')
+    # tweet = api.get_status(1448380249998303232)
+    # l = shotter.screenshot(tweet)
+    # os.rename(l, "tests/quoted_deleted_tweet.png")
 
-# # reply on retweet with video
-# print('reply on retweet with video')
-# tweet = api.get_status(1454912491406905358)
-# l = shotter.screenshot(tweet)
-# os.rename(l, "tests/reply_on_retweet_with_video.png")
+    # # reply on retweet with video
+    # print('reply on retweet with video')
+    # tweet = api.get_status(1454912491406905358)
+    # l = shotter.screenshot(tweet)
+    # os.rename(l, "tests/reply_on_retweet_with_video.png")
 
-print('long thread conversation tweet')
-tweet = api.get_status(1462773987407060996)
-l = shotter.screenshot(tweet)
-os.rename(l, "tests/long_thread_conversation_tweet.png")
+    print('long thread conversation tweet')
+    tweet = api.get_status(1462773987407060996)
+    l = shotter.screenshot(tweet)
+    os.rename(l, "tests/long_thread_conversation_tweet.png")
+
+def tweet_test():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    rate_limit = api.rate_limit_status()
+    print(rate_limit)
+    tweet = api.update_status("Ja wat")
+    print(tweet)
+
+tweet_test()
